12fdd0c4fe218c7de1eb24d3d38e5404b9790513
Jeremy Landis <jeremylandis@hotmail.com>
2023-12-20 15:33:30 -0500

    [ci] Remove unused import

--- src/main/java/org/eluder/coveralls/maven/plugin/CoverallsReportMojo.java
+++ src/main/java/org/eluder/coveralls/maven/plugin/CoverallsReportMojo.java
@@ -26,7 +26,6 @@ package org.eluder.coveralls.maven.plugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
