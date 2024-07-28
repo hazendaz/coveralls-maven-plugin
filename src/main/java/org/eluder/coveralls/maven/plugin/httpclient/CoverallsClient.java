311859a0acc55c15382d4191c2c50cd72996e050
Jeremy Landis <jeremylandis@hotmail.com>
2024-07-28 19:34:57 -0400

    [utf] Use standardCharsets utf 8

--- src/main/java/org/eluder/coveralls/maven/plugin/httpclient/CoverallsClient.java
+++ src/main/java/org/eluder/coveralls/maven/plugin/httpclient/CoverallsClient.java
@@ -30,6 +30,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.security.Provider;
 import java.security.Security;
 
@@ -56,7 +57,7 @@ public class CoverallsClient {
     }
 
     private static final String FILE_NAME = "coveralls.json";
-    private static final ContentType MIME_TYPE = ContentType.create("application/octet-stream", "utf-8");
+    private static final ContentType MIME_TYPE = ContentType.create("application/octet-stream", StandardCharsets.UTF_8);
 
     private final String coverallsUrl;
     private final HttpClient httpClient;
