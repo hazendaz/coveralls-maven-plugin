ebad17a40e16ed478a9e3a7416feee107834168b
Jeremy Landis <jeremylandis@hotmail.com>
2025-10-04 13:58:28 -0400

    Add finals

--- src/test/java/org/eluder/coveralls/maven/plugin/CoverallsReportMojoTest.java
+++ src/test/java/org/eluder/coveralls/maven/plugin/CoverallsReportMojoTest.java
@@ -447,7 +447,7 @@ class CoverallsReportMojoTest {
      * @param fixture
      *            the fixture
      */
-    static void verifySuccessfulSubmit(Log logMock, List<List<String>> fixture) {
+    static void verifySuccessfulSubmit(final Log logMock, final List<List<String>> fixture) {
         Mockito.verify(logMock, Mockito.times(1))
                 .info("Gathered code coverage metrics for " + CoverageFixture.getTotalFiles(fixture)
                         + " source files with " + CoverageFixture.getTotalLines(fixture) + " lines of code:");
